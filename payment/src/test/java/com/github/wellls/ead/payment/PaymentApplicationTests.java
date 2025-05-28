2e6aed27ef3ac1317c8419de54ffc0c39e44e968
Wellington <wellington.map@hotmail.com>
2025-05-28 14:56:21 -0300

    chore(payment): start payment project

---
+++ payment/src/test/java/com/github/wellls/ead/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wellls.ead.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
