c6c616a14074ba16cfa82c9d3e88928a20fd6988
Wellington <wellington.map@hotmail.com>
2025-06-02 09:39:36 -0300

    chore(authuser): add global CORS configuration with maxAge 3600s

--- authuser/src/main/java/com/github/wellls/ead/authuser/configs/ResolverConfig.java
+++ authuser/src/main/java/com/github/wellls/ead/authuser/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,7 +15,11 @@ public class ResolverConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         var pageableResolver = new PageableHandlerMethodArgumentResolver();
         pageableResolver.setFallbackPageable(PageRequest.of(0, 2));
-
         argumentResolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").maxAge(3600);
     }
 }
